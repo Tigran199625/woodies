3c69795bb380788ff69b62f03c575c7e3cc8cfd8
Tigran199625 <5511155111t@gmail.com>
2021-12-22 06:18:15 -0800

    fix

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 let express = require("express");
 let app = express();
 const db = require('./models');
+let bodyparser = require('body-parser')
+let urlencodedParser = bodyparser.urlencoded({extended:false})
 app.set("view engine", "ejs");
 
 const { mailingUsers } = require('./models')
+
+app.use(express.static(__dirname + 'assets'))
+
+app.get("/", (req, res) => {
+    res.render('main')
+})
+
+app.post("/", urlencodedParser, (req, res) => {
+    response = {
+        mail : req.body.mail
+    }
+    res.send(mail)
+})
 
 mailingUsers.sync().then((data) => {
     console.log("Table success");
